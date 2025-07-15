1a0234e53ba18c9029088a222900b3c6ab1c55ac
IceP0tato <rlawodud0313@gmail.com>
2025-07-15 16:43:22 +0900

    0715_AppStart 최초 화면 구현

--- src/AppStart.java
+++ src/AppStart.java
@@ -1,2 +1,7 @@
+import view.LibraryView;
+
 public class AppStart {
+    public static void main(String[] args) {
+        LibraryView.getInstance().index();
+    }
 }
--- src/view/LibraryView.java
+++ src/view/LibraryView.java
@@ -1,10 +1,34 @@
 package view;
 
+import java.util.Scanner;
+
 public class LibraryView {
+    private Scanner scan = new Scanner(System.in);
 
     private LibraryView() {}
     private static final LibraryView instance = new LibraryView();
     public static LibraryView getInstance() {
         return instance;
+    }
+
+    public void index() {
+        for (;;) {
+            System.out.println("=========== 도서관리 시스템 ===========");
+            System.out.println("  1.회원가입 | 2.로그인");
+            System.out.println("==========================================");
+            System.out.print("선택 > ");
+            int num = scan.nextInt();
+
+            if (num == 1) signUp();
+            else if (num == 2) signIn();
+        }
+    }
+
+    public void signUp() {
+
+    }
+
+    public void signIn() {
+
     }
 }
